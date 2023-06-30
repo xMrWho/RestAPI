20c5356356116b87bbd616c1627755d0a2f53584
xMrWho <ti19031997@googlemail.com>
2023-06-30 13:03:14 +0200

    ujbikuiluikiluo

---
+++ methods/relationships/getList.js
@@ -0,0 +1,23 @@
+module.exports = function getRelationshipList(database, usedDatabase, parameters) {
+    return new Promise(async function (resolve, reject) {
+      try {
+        switch (usedDatabase) {
+          case "MongoDB": {
+            resolve({ error: "Not implemented yet" });
+          }
+          case "MySQL": {
+          }
+          default: {
+            resolve({ error: "Not implemented yet" });
+          }
+        }
+      } catch (error) {
+        resolve({
+          error: "Error retrieving data from database",
+          msg: error.message,
+          stack: error.stack,
+        });
+      }
+    });
+  };
+  
